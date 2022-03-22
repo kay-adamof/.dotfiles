6680ef8ccbc62bea951142d89045f242c8b44bdf
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2022-03-22 11:46:52 +0900

    Add new configuration file for fish shell

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+
+
+
+
+# ================================================================================
+#     end of file
+# ================================================================================
+# [Starship: Cross-Shell Prompt](https://starship.rs/#fish)
+# Add the following to the end of ~/.config/fish/config.fish:
+starship init fish | source
+ 
